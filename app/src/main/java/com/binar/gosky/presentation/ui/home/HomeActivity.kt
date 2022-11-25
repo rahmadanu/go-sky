ab18cedd7a6da737d401a95494f8dec0cd8b3719
rahmadanu <sadewarahmadanu@gmail.com>
2022-11-26 02:29:14 +0700

    Create initial bottom navigation and add dependencies

---
+++ app/src/main/java/com/binar/gosky/presentation/ui/home/HomeActivity.kt
@@ -0,0 +1,60 @@
+package com.binar.gosky.presentation.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.binar.gosky.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var bottomNav: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        bottomNav = findViewById(R.id.bottom_nav_view)
+
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.homeFragment,
+            R.id.wishlistFragment,
+            R.id.profileFragment
+        ).build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNav.setupWithNavController(navController)
+        supportActionBar?.hide()
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+/*                R.id.detailFragment -> hideBottomNav(true)
+                R.id.updateProfileFragment -> hideBottomNav(true)*/
+                else -> hideBottomNav(false)
+            }
+        }
+    }
+
+    private fun hideBottomNav(hide: Boolean) {
+        if (hide) {
+            bottomNav.visibility = View.GONE
+        } else {
+            bottomNav.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
