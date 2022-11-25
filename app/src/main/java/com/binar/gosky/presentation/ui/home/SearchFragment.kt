d7ea4662ef2f81611744faca300ef17efa24ffbe
rahmadanu <sadewarahmadanu@gmail.com>
2022-11-26 00:49:04 +0700

    delete onDestroy

--- app/src/main/java/com/binar/gosky/presentation/ui/home/SearchFragment.kt
+++ app/src/main/java/com/binar/gosky/presentation/ui/home/SearchFragment.kt
@@ -18,8 +18,4 @@ class SearchFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-
 }
