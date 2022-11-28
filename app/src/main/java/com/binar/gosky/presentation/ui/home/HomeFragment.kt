3887180a89b7dfb1a7b96d7d31fc03491f5116a7
rahmadanu <sadewarahmadanu@gmail.com>
2022-11-28 21:31:32 +0700

    Implement DatePickerDialog when date EditText is clicked

--- app/src/main/java/com/binar/gosky/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/binar/gosky/presentation/ui/home/HomeFragment.kt
@@ -1,19 +1,75 @@
 package com.binar.gosky.presentation.ui.home
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.binar.gosky.R
+import com.binar.gosky.databinding.FragmentHomeBinding
+import java.util.*
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.etDepartureDate.setOnClickListener {
+            showDatePickerDialog(it.id)
+            Log.d("id", "departure: ${it.id}")
+        }
+        binding.etReturnDate.setOnClickListener {
+            Log.d("id", "return: ${it.id}")
+            showDatePickerDialog(it.id)
+        }
+    }
+
+    private fun showDatePickerDialog(id: Int) {
+
+        val datePickerDialog = DatePickerDialog(
+            requireContext(),
+            { view, year, monthOfYear, dayOfMonth ->
+                day = dayOfMonth
+                val formattedMonth = listOf("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Des")
+                when (id) {
+                    binding.etDepartureDate.id -> {
+                        binding.etDepartureDate.setText("$dayOfMonth ${formattedMonth.get(monthOfYear)}, $year")
+                    }
+                    binding.etReturnDate.id -> {
+                        binding.etReturnDate.setText("$dayOfMonth ${formattedMonth.get(monthOfYear)}, $year")
+                    }
+                }
+            },
+            year,
+            month,
+            day
+        )
+        datePickerDialog.show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        private val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        private var day = calendar.get(Calendar.DAY_OF_MONTH)
     }
 }
