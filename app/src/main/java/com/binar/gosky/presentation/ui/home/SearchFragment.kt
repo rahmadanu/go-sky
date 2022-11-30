2e3ec67244b30d31d1fe6b4bf26bc62f4d64dc57
ammaar <muhammadfarroos12@gmail.com>
2022-11-30 20:52:15 +0700

    test error

--- app/src/main/java/com/binar/gosky/presentation/ui/home/SearchFragment.kt
+++ app/src/main/java/com/binar/gosky/presentation/ui/home/SearchFragment.kt
@@ -10,12 +10,8 @@ import com.binar.gosky.R
 
 class SearchFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    override fun onStart() {
+        super.onStart()
     }
 
 }
