71a0483192d828de3cf7ee48ef5f67e8afa68479
rahmadanu <sadewarahmadanu@gmail.com>
2022-12-01 03:26:15 +0700

    Provide DI data source & repository module and @HiltAndroidApp

--- app/src/main/java/com/binar/gosky/data/repository/TicketsRepository.kt
+++ app/src/main/java/com/binar/gosky/data/repository/TicketsRepository.kt
@@ -2,16 +2,26 @@ package com.binar.gosky.data.repository
 
 import com.binar.gosky.data.network.datasource.TicketsRemoteDataSource
 import com.binar.gosky.data.network.model.search.Tickets
+import com.binar.gosky.wrapper.Resource
 import javax.inject.Inject
 
 interface TicketsRepository {
-    suspend fun getTickets(): Tickets
+    suspend fun getTickets(): Resource<Tickets>
 }
 
 class TicketsRepositoryImpl @Inject constructor(private val dataSource: TicketsRemoteDataSource) :
     TicketsRepository {
-    override suspend fun getTickets(): Tickets {
-        return dataSource.getTickets()
+    override suspend fun getTickets(): Resource<Tickets> {
+        return proceed {
+            dataSource.getTickets()
+        }
     }
 
+    private suspend fun <T> proceed(coroutines: suspend () -> T): Resource<T> {
+        return try {
+            Resource.Success(coroutines.invoke())
+        } catch (e: Exception) {
+            Resource.Error(e)
+        }
+    }
 }
---
+++ app/src/main/java/com/binar/gosky/wrapper/Resource.kt
@@ -0,0 +1,12 @@
+package com.binar.gosky.wrapper
+
+sealed class Resource<T>(
+    val payload: T? = null,
+    val message: String? = null,
+    val exception: Exception? = null
+) {
+    class Success<T>(val data: T): Resource<T>(data)
+    class Empty<T>(data: T? = null): Resource<T>(data)
+    class Error<T>(exception: Exception?, data: T? = null): Resource<T>(data, exception = exception)
+    class Loading<T>(data: T? = null): Resource<T>(data)
+}
