a1e6970bd66ec0a60d3dcfd9373a7670a65da08f
ammaar <muhammadfarroos12@gmail.com>
2022-12-01 23:22:49 +0700

    add text departure

--- app/src/main/java/com/binar/gosky/presentation/ui/home/SearchResultFragment.kt
+++ app/src/main/java/com/binar/gosky/presentation/ui/home/SearchResultFragment.kt
@@ -15,7 +15,7 @@ class SearchResultFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        return inflater.inflate(R.layout.fragment_search_result, container, false)
     }
 
 }
