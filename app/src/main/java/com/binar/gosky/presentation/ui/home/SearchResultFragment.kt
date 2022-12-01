acb7412045899a6501ab372943e1b4d6329f07c3
ammaar <muhammadfarroos12@gmail.com>
2022-12-01 23:10:16 +0700

    Layouting fragment_search.xml

--- app/src/main/java/com/binar/gosky/presentation/ui/home/SearchResultFragment.kt
+++ app/src/main/java/com/binar/gosky/presentation/ui/home/SearchResultFragment.kt
@@ -8,7 +8,7 @@ import android.view.ViewGroup
 import com.binar.gosky.R
 
 
-class SearchFragment : Fragment() {
+class SearchResultFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
