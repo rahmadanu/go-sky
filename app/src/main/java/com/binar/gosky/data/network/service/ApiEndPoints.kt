d44bcb41b4547d157c47a03a46180bd996e840be
rahmadanu <sadewarahmadanu@gmail.com>
2022-12-09 21:08:50 +0700

    Set up register and login function in api service, model, and endpoint

--- app/src/main/java/com/binar/gosky/data/network/service/ApiEndPoints.kt
+++ app/src/main/java/com/binar/gosky/data/network/service/ApiEndPoints.kt
@@ -1,6 +1,11 @@
 package com.binar.gosky.data.network.service
 
 object ApiEndPoints {
+    // Tickets
     const val GET_TICKETS_ENDPOINT = "tickets"
+
+    // Auth
     const val GET_OTP = "auth/otp"
+    const val POST_REGISTER = "auth/register"
+    const val POST_LOGIN = "auth/login"
 }
--- app/src/main/java/com/binar/gosky/data/network/service/AuthApiService.kt
+++ app/src/main/java/com/binar/gosky/data/network/service/AuthApiService.kt
@@ -1,7 +1,12 @@
 package com.binar.gosky.data.network.service
 
+import com.binar.gosky.data.network.model.auth.login.LoginRegisterRequestResponse
+import com.binar.gosky.data.network.model.auth.login.LoginRequestBody
 import com.binar.gosky.data.network.model.auth.otp.OtpResponse
+import com.binar.gosky.data.network.model.auth.register.RegisterRequestBody
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface AuthApiService {
@@ -10,4 +15,14 @@ interface AuthApiService {
     suspend fun getOtp(
         @Query("email") email: String
     ): OtpResponse
+
+    @POST(ApiEndPoints.POST_REGISTER)
+    suspend fun postRegisterUser(
+        @Body registerRequestBody: RegisterRequestBody
+    ): LoginRegisterRequestResponse
+
+    @POST(ApiEndPoints.POST_LOGIN)
+    suspend fun postLoginUser(
+        @Body loginRequestBody: LoginRequestBody
+    ): LoginRegisterRequestResponse
 }
