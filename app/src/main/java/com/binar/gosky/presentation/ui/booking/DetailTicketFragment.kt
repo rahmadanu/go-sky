d5cd0c93a9c2bd9166f5cb410682c18112b41045
rahmadanu <sadewarahmadanu@gmail.com>
2022-12-20 17:42:46 +0700

    Remove up button in order detail fragment

--- app/src/main/java/com/binar/gosky/presentation/ui/booking/DetailTicketFragment.kt
+++ app/src/main/java/com/binar/gosky/presentation/ui/booking/DetailTicketFragment.kt
@@ -3,22 +3,20 @@ package com.binar.gosky.presentation.ui.booking
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.binar.gosky.R
 import com.binar.gosky.data.network.model.tickets.TicketsItem
 import com.binar.gosky.data.network.model.transactions.new_transaction.NewTransactionData
-import com.binar.gosky.data.network.model.users.data.UserByIdData
 import com.binar.gosky.databinding.FragmentDetailTicketBinding
 import com.binar.gosky.util.ConvertUtil
-import com.binar.gosky.wrapper.Resource
 import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -52,7 +50,6 @@ class DetailTicketFragment : Fragment() {
 
     private fun setOnClickListener() {
         binding.apply {
-            ivBack.setOnClickListener { findNavController().popBackStack() }
             btnBackToHome.setOnClickListener { findNavController().navigate(R.id.action_detailTicketFragment_to_homeFragment) }
         }
     }
