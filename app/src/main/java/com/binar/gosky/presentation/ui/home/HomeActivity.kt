a1dfe208fa7b8dbde0db80699d5a409ff7f88833
RAHMAD <rahmadkombeli001@gmail.com>
2023-01-02 01:39:56 -0800

    menu bar update

--- app/src/main/java/com/binar/gosky/presentation/ui/home/HomeActivity.kt
+++ app/src/main/java/com/binar/gosky/presentation/ui/home/HomeActivity.kt
@@ -18,6 +18,7 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var bottomNav: BottomNavigationView
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
