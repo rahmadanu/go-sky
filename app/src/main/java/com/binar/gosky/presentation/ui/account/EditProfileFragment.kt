c34bfbbf614269ef27ba763a07773a45c0f9d7c9
rahmadanu <sadewarahmadanu@gmail.com>
2023-01-02 17:07:59 +0700

    Fix loading state in edit profile image fragment

--- app/src/main/java/com/binar/gosky/presentation/ui/account/EditProfileFragment.kt
+++ app/src/main/java/com/binar/gosky/presentation/ui/account/EditProfileFragment.kt
@@ -78,14 +78,14 @@ class EditProfileFragment : Fragment() {
                     binding.pbLoadingImage.isVisible = false
                     binding.tvAddImage.isVisible = false
                     binding.tvDeleteImage.isVisible = true
-                    binding.ivImage.visibility = View.INVISIBLE
+                    binding.ivImage.isVisible = true
                 }
                 is Resource.Error -> {
                     Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
                 }
                 is Resource.Loading -> {
                     binding.pbLoadingImage.isVisible = true
-                    binding.ivImage.isVisible = false
+                    binding.ivImage.visibility = View.INVISIBLE
                 }
                 else -> {}
             }
